4fe93f1053af900fe7931520eed18dfd83550af2
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-09-29 16:15:47 +0200

    Update query.ts

--- query.ts
+++ query.ts
@@ -1,1 +1,15 @@
+import { createConnection } from 'mysql';
 
+const database = createConnection(/* Put your database informations here */);
+database.connect((error) => {
+  if (error) throw error;
+});
+
+export const query = (search: string) => {
+  return new Promise<string | any[]>((resolve, reject) => {
+    database.query(search, (error, request) => {
+      if (error) reject(error)
+      else resolve(request);
+    });
+  });
+};
