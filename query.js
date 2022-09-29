da59ec14ca6b0f00f1a20de38c40a9f0060da7ea
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-09-29 16:13:00 +0200

    Update query.js

--- query.js
+++ query.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql');
 
+const database = mysql.createDatabase(/* Put your mysql informations here*/);
+database.connect((error) => {
+  if (error) throw error;
+});
+
+module.exports.query = (query) => {
+  return new Promise((resolve, reject) => {
+    database.query(resolve, (error, request) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(request)
+      }
+    });
+  })
+};
